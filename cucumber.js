d5f030897eee35c7f22dabdf1f40368d931b70f7
Premdevi9091 <premdevi9091@gmail.com>
2026-03-07 14:45:22 +0530

    modified folder path and added tags configuration

--- cucumber.js
+++ cucumber.js
@@ -1,14 +1,14 @@
 module.exports = {
     default: {
         require: [
-            "src/step-definitions/*.ts",
-            "support/hooks/*.ts"
+            "tests/step-definitions/**/*.ts",
+            "support/hooks/**/*.ts"
         ],
         format: [
             "progress",
             "json:test-reports/cucumber-report.json"
         ],
         requireModule: ["ts-node/register"],
-        paths: ["src/features/*.feature"]
+        paths: ["tests/features/**/*.feature"]
     }
 };
