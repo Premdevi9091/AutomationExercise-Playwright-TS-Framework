ada3ec8fdfccbd5298852c3694d4a80e1820035a
Premdevi9091 <premdevi9091@gmail.com>
2026-03-13 20:20:43 +0530

    added "add product in cart" testcase

--- tests/features/products.feature
+++ tests/features/products.feature
@@ -1,14 +1,14 @@
-@products
-Feature: Verify Products functionality
+@products @smoke
+Feature: Validate Products functionality
 
-Scenario: Verify list of Products
+Scenario: List of Products
     Given User navigates to Automation Exercise website
     Then Home page should be visible
     When User click on Product button
     Then User should navigated to All Product page
     And click on view products and verify details
 
-Scenario Outline: Verify search products
+Scenario Outline: Search products
     Given User navigates to Automation Exercise website
     Then Home page should be visible
     When User click on Product button
@@ -20,3 +20,22 @@ Examples:
 | product_name |
 | top |
 | shirt |
+
+Scenario: Add product in cart
+    Given User navigates to Automation Exercise website
+    Then Home page should be visible
+    When User click on Product button
+    Then User should navigated to All Product page
+    And hover on the "1" product
+    And click on Add to cart
+    And click on continue shopping
+    And hover on the "4" product
+    And click on Add to cart
+    And click on continue shopping
+    And hover on the "2" product
+    And click on Add to cart
+    And click on continue shopping
+    And hover on the "2" product
+    And click on Add to cart
+    And click on view cart
+    And verify the products in cart
--- tests/pages/Products.ts
+++ tests/pages/Products.ts
@@ -2,13 +2,14 @@ import { BasePage } from "./BasePage";
 import { UIActions } from "../../utils/UIActions";
 import { expect, Page } from "@playwright/test";
 import { TestLogger } from "../../utils/testlogger";
-import { UserDataManager } from "../../utils/UserDataManager";
-import { DataGenerator } from "../../utils/generateRandom";
+import logger from "../../utils/logger";
 
 
 export class Products extends BasePage{
     private actions: UIActions | undefined;
     private testLogger: TestLogger;
+    private product_no : string | undefined;
+
 
     constructor(page: Page, testLogger: TestLogger){
         super(page);
@@ -24,6 +25,11 @@ export class Products extends BasePage{
     private searchBar = this.page.locator("input#search_product");
     private searchBtn = this.page.locator("button#submit_search");
     private productNames = this.page.locator(".productinfo p");
+    private hoverEle = this.page.locator(".product-image-wrapper");
+    private continueShoppingBtn = this.page.getByText('Continue Shopping');
+    private viewCart = this.page.getByText('View Cart');
+    private modelAddedText = this.page.locator('.modal-content h4');
+    private modelAddedMsg = this.page.locator('.modal-content p').first();
 
     
     async clickProduct(){
@@ -36,15 +42,15 @@ export class Products extends BasePage{
     }
 
     async clickOnViewProduct(){
-        for(let i = 0; i < 10; i++){
+        for(let i = 0; i < 5; i++){
             const viewProducts = this.page.locator("a[href*='product_details']");
             await this.actions?.click(viewProducts.nth(i), `Clicked on View Product for: ${i + 1} product`);
-            await this.verifyProductDetails(i + 1);
+            await this.verifyProductDetails();
             await this.actions?.click(this.productLink, "Products");
         }
     }
 
-    async verifyProductDetails(product_id: any){
+    async verifyProductDetails(){
         let productsDetails: Record<string, any> = {};
         
         const name = await this.actions?.getText(this.productInformation.locator("h2"), "Product Name");
@@ -62,7 +68,7 @@ export class Products extends BasePage{
             Condition: condition,
             Brand: brand
         }
-        this.testLogger.put(`product_${product_id}`, productsDetails);        
+        this.testLogger.put(`Products`, productsDetails);        
     }
 
     async searchProduct(product_name : string){
@@ -87,5 +93,79 @@ export class Products extends BasePage{
         }
         expect(matchFound).toBeTruthy();
         this.testLogger.put(`SearchProducts_${product_name}`, searchProducts);
+    }
+
+    async hoverOnProduct(product_number: any){
+        this.product_no = product_number;
+        const hover = this.hoverEle.nth(product_number - 1);
+        await this.actions?.hoverElement(hover, `Product ${product_number}`);
+        let productsDetails: Record<string, any> = {};
+        
+        const name = await this.actions?.getText(hover.locator(".productinfo p"), "Product Name");
+        const price = await this.actions?.getText(hover.locator(".productinfo h2"), "Product Price");
+        productsDetails = {
+            product_id: product_number,
+            name: name,
+            price: price
+        }
+        this.testLogger.put(`Products`, productsDetails);
+
+    }
+    async clickAddtoCart(){
+        const addtoCart = this.page.locator(`.overlay-content a[data-product-id='${this.product_no}']`);
+        await this.actions?.click(addtoCart, "Add to cart");
+        await this.actions?.assertText(this.modelAddedText, "Added!", "Added Text");
+        await this.actions?.assertText(this.modelAddedMsg, "Your product has been added to cart.", "Success Message for cart");
+    }
+
+    async clickContinueShopping(){
+        await this.actions?.click(this.continueShoppingBtn, "Continue Shopping");
+    }
+
+    async clickViewCart(){
+        await this.actions?.click(this.viewCart, "View Cart");
+    }
+
+    async verifyCartDetails(){
+        const expectedDetails = this.testLogger.get("Products");
+        const expectedDetailsMap = new Map();
+
+        //Group products by product_id and calculate quantity
+        for(const product of expectedDetails){
+            if(!expectedDetailsMap.has(product.product_id)){
+                expectedDetailsMap.set(product.product_id, {
+                     name: product.name,
+                     price: product.price,
+                     quantity: 1
+                });
+            } else{
+                expectedDetailsMap.get(product.product_id).quantity += 1;
+            }
+        }
+        logger.info(`Expected cart map: ${JSON.stringify([...expectedDetailsMap])}`);
+        
+        for(const [productId, expected] of expectedDetailsMap){
+            const row = this.page.locator(`#product-${productId}`);
+            const qty = expected.quantity;
+
+            //convert price "Rs. 500 -> 500"
+            const priceValue = Number(expected.price.replace(/[^\d]/g, ""));
+            const expectedTotal: any = priceValue * qty;
+
+            //Format back to UI format
+            const expectedTotalText = `Rs. ${expectedTotal}`;
+
+            const productNameEle = row.locator(`td.cart_description h4`);
+            await this.actions?.compareText(productNameEle, expected.name, `Product ${productId} : Name`);
+
+            const productPriceEle = row.locator(`td.cart_price p`);
+            await this.actions?.compareText(productPriceEle, expected.price, `Product ${productId} : Price`);
+
+            const productQtyEle= row.locator(`td.cart_quantity button`);
+            await this.actions?.compareText(productQtyEle, qty.toString(), `Product ${productId} : Quantity`);
+
+            const productTotalPriceEle= row.locator(`td.cart_total p`);
+            await this.actions?.compareText(productTotalPriceEle, expectedTotalText, `Product ${productId} : Total price`);
+        }
     }
 }
