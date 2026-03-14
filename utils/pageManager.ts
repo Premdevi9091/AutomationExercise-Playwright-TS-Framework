a0fee7e6cd4c73bec1622b0de47499ee6d2f1ad0
Premdevi9091 <premdevi9091@gmail.com>
2026-03-14 16:02:07 +0530

    add "PageManager" to handle pages

---
+++ utils/pageManager.ts
@@ -0,0 +1,23 @@
+import { Page } from "@playwright/test";
+import { TestLogger } from "./testlogger";
+
+export class PageManager{
+
+    private page: Page;
+    private logger: TestLogger;
+    private pages: Record<string, any> = {};
+
+    constructor(page: Page, logger: TestLogger){
+        this.page = page;
+        this.logger = logger;
+    }
+
+    get<T> (PageClass: new (page: Page, logger: TestLogger) => T): T{
+        const pageName = PageClass.name;
+
+        if(!this.pages[pageName]){
+            this.pages[pageName] = new PageClass(this.page, this.logger);
+        }
+        return this.pages[pageName];
+    }
+}
--- world/customWorld.ts
+++ world/customWorld.ts
@@ -1,15 +1,15 @@
 import { setWorldConstructor, World } from "@cucumber/cucumber";
 import { Browser, BrowserContext, Page} from "@playwright/test";
 import { TestLogger } from "../utils/testlogger";
+import { PageManager } from "../utils/pageManager";
 
 export class CustomWorld extends World{
     browser!: Browser;
     context!: BrowserContext;
     page!: Page;
-
+    pages!: PageManager;
     scenarioName!: string;
     testData: any = {};
-
     testLogger!: TestLogger;
 }
 setWorldConstructor(CustomWorld);
